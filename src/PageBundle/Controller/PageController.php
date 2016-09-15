2fab0f37af6f3ed58b287fb9a7d2466caa69d1e1
MikuraAngels <solene.ernoul@gmail.com>
2016-09-15 11:32:23 +0200

    Modification CSS, création des pages d'infos

--- src/PageBundle/Controller/PageController.php
+++ src/PageBundle/Controller/PageController.php
@@ -15,4 +15,30 @@ class PageController extends Controller
     {
         return $this->render('', array('page' => $page));
     }
+
+
+    /**
+     * Conditions d'Utilisations
+     */
+    public function cguAction()
+    {
+        return $this->render('PageBundle:Page:conditions-utilisations.html.twig');
+    }
+
+    /**
+     * Mentions Légales
+     */
+    public function mlAction()
+    {
+        return $this->render('PageBundle:Page:mentions-legales.html.twig');
+    }
+
+    /**
+     * FAQ
+     */
+    public function faqAction()
+    {
+        return $this->render('PageBundle:Page:faq.html.twig');
+    }
+
 }
