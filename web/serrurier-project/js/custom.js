1ede28c24d3698ad767bdf18d7d39b10ed2a2019
llyude <ludovic.sire@gmail.com>
2016-10-14 16:23:10 +0200

    Modification Planning/ suppression entité -> array

--- web/serrurier-project/js/custom.js
+++ web/serrurier-project/js/custom.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     // Initialisation du Calendrier
 
+    console.log('test');
+
     $('#calendar').fullCalendar({
         dayClick: function() {
             alert('a day has been clicked!');
